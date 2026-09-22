ab28c4f0769f5a8fd1c1d67a2f6e5a098a5a4f76
Elliot Wareham <elliot.wareham@outlook.com>
2026-09-22 21:32:12 +0100

    Needs Attention section updates dynamically with JS

--- script.js
+++ script.js
@@ -1,63 +1,78 @@
 const cocktails = [
     {
         name: "Mai Tai",
-        mastery: 42
+        mastery: 65,
+        lastPractised: "5 days ago"
     },
     {
         name: "Bramble",
-        mastery: 88
+        mastery: 88,
+        lastPractised: "3 days ago"
     },
     {
         name: "Old Fashioned",
-        mastery: 100
+        mastery: 60,
+        lastPractised: "2 days ago"
     },
     {
         name: "Margarita",
-        mastery: 100
+        mastery: 100,
+        lastPractised: "3 days ago"
     },
     {
         name: "Espresso XO",
-        mastery: 100
+        mastery: 60,
+        lastPractised: "3 days ago"
     },
     {
         name: "Napalm Skies",
-        mastery: 33
+        mastery: 60,
+        lastPractised: "3 days ago"
     },
     {
         name: "Pornstar Martini",
-        mastery: 100
+        mastery: 84,
+        lastPractised: "5 days ago"
     },
     {
         name: "Kinder Bueno",
-        mastery: 100
+        mastery: 100,
+        lastPractised: "3 days ago"
     },
     {
         name: "Bad Habits",
-        mastery: 80
+        mastery: 80,
+        lastPractised: "2 days ago"
     },
     {
-        name: "The Big Green One",
-        mastery: 84
+        name: "The Green One",
+        mastery: 44,
+        lastPractised: "3 days ago"
     },
     {
         name: "Drumstick",
-        mastery: 90
+        mastery: 90,
+        lastPractised: "5 days ago"
     },
     {
         name: "White Russian",
-        mastery: 52
+        mastery: 42,
+        lastPractised: "2 days ago"
     },
     {
         name: "Aperol Spritz",
-        mastery: 90
+        mastery: 90,
+        lastPractised: "5 days ago"
     },
     {
         name: "Jammy Dodger",
-        mastery: 100
+        mastery: 100,
+        lastPractised: "3 days ago"
     },
     {
         name: "Hugo Spritz",
-        mastery: 80
+        mastery: 80,
+        lastPractised: "2 days ago"
     }
 ];
 
@@ -91,9 +106,44 @@ for (const cocktail of cocktails) {
     masteryInfoPercentage.textContent = `${cocktail.mastery}%`;
     progressBarFill.style.width = `${cocktail.mastery}%`;
     if (cocktail.mastery >= 100) {
-    progressBarFill.classList.add("fill-100")
+        progressBarFill.classList.add("fill-100")
     };
     carousel.appendChild(card);
 };
 
+// needs attention section
 
+const attentionCardsList = document.querySelector(".attention-list");
+
+for (const cocktail of cocktails) {
+    if (cocktail.mastery < 60) {
+        const attentionCard = document.createElement("div");
+        const attentionCardHeading = document.createElement("h3");
+        const masteryAttentionSection = document.createElement("div");
+        const masteryAttentionSectionInfo = document.createElement("div");
+        const masteryText = document.createElement("span");
+        const masteryPercentage = document.createElement("span");
+        const masteryAttentionSectionProgressBar = document.createElement("div");
+        const masteryAttentionSectionProgressBarFill = document.createElement("div");
+        const lastTimePractised = document.createElement("p");
+        masteryAttentionSection.classList.add("mastery-section");
+        masteryAttentionSectionInfo.classList.add("mastery-info");
+        masteryAttentionSectionProgressBar.classList.add("progress-bar");
+        attentionCard.classList.add("attention-card");
+        masteryAttentionSectionProgressBarFill.classList.add("progress-fill");
+        attentionCardsList.appendChild(attentionCard);
+        attentionCard.appendChild(attentionCardHeading);
+        attentionCard.appendChild(masteryAttentionSection);
+        masteryAttentionSection.appendChild(masteryAttentionSectionInfo);
+        masteryAttentionSectionInfo.appendChild(masteryText);
+        masteryAttentionSectionInfo.appendChild(masteryPercentage);    
+        masteryAttentionSection.appendChild(masteryAttentionSectionProgressBar);
+        masteryAttentionSectionProgressBar.appendChild(masteryAttentionSectionProgressBarFill);
+        attentionCard.appendChild(lastTimePractised);
+        attentionCardHeading.textContent = cocktail.name;
+        masteryText.textContent = "Mastery";
+        masteryPercentage.textContent = `${cocktail.mastery}%`;
+        masteryAttentionSectionProgressBarFill.style.width = `${cocktail.mastery}%`;
+        lastTimePractised.textContent = `Last practised: ${cocktail.lastPractised}`;
+    }
+};
